5ee3b89e66b26f741f3eee124fa05434b6a0ffaa
Trần Xuân Tiến <tranxuantienvtqs@gmail.com>
2025-07-28 09:11:12 +0700

    first commit

---
+++ src/main/java/com/example/springboot_education/SpringbootEducationApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_education;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEducationApplication.class, args);
+	}
+
+}
